d6bcdc8d81c8a3b64366de095fa8aa50182a54af
Badlop <badlop@process-one.net>
2019-01-09 10:53:15 +0100

    Update copyright to 2019 (processone/ejabberd#2756)

--- src/p1_file_queue.erl
+++ src/p1_file_queue.erl
@@ -1,6 +1,6 @@
 %%%-------------------------------------------------------------------
 %%% @author Evgeny Khramtsov <ekhramtsov@process-one.net>
-%%% @copyright (C) 2017, Evgeny Khramtsov
+%%% @copyright (C) 2017-2019 Evgeny Khramtsov
 %%% @doc
 %%%
 %%% @end
